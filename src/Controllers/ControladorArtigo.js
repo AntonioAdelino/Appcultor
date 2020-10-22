e8d4c76bcd663ecc875eea57d0f1e9dda6727406
Victor-Andrade <victor.hugooo@hotmail.com.br>
2020-10-22 12:21:15 -0300

    Otimização da consulta na API para tratamento de erros

--- src/Controllers/ControladorArtigo.js
+++ src/Controllers/ControladorArtigo.js
@@ -1,14 +1,25 @@
-export default async function buscarArtigo(texto){
- 
-return fetch("http://173.82.232.87:3001/api/article/searchArticle?text="+texto)
-  .then(res => res.json())
-  
+import { ToastAndroid } from "react-native"
+
+export default async function buscarArtigo(texto) {
+
+    return fetch("http://173.82.232.87:3001/api/article/searchArticle?text=" + texto)
+        .then(response => {
+            // valida se a requisição falhou
+            if (!response.ok) {
+                return new Error('falhou a requisição') // cairá no catch da promise
+            }
+
+            // verificando pelo status
+            if (response.status === 404) {
+                return new Error('não encontrou qualquer resultado')
+            }
+
+            // retorna uma promise com os dados em JSON
+            return response.json()
+        })
 
 
-  //fazendo consulta na api
-  /*var request = await fetch("http://173.82.232.87:3001/api/article/searchArticle?text="+texto)
-  var resposta = await request.json();
-  
-  
-  return resposta;*/
+    // Comando usado para implementação inicial
+    //.then(res => res.json())
+
 }
