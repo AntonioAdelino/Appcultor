2af79466dc87642f475eb4c6c2e5d2abd507e7f9
AntonioAdelino <antonio.asn03@gmail.com>
2020-10-28 10:25:32 -0300

    Controlador de Flores com busca concluido

---
+++ src/Controllers/controladorFlor.js
@@ -0,0 +1,25 @@
+import { ToastAndroid } from "react-native"
+
+export default async function buscarFlor(texto) {
+
+    return fetch("http://173.82.232.87:3001/api/flower/searchFlower?text=" + texto)
+        .then(response => {
+            // valida se a requisição falhou
+            if (!response.ok) {
+                return new Error('falhou a requisição') // cairá no catch da promise
+            }
+
+            // verificando pelo status
+            if (response.status === 404) {
+                return new Error('não encontrou qualquer resultado')
+            }
+
+            // retorna uma promise com os dados em JSON
+            return response.json()
+        })
+
+
+    // Comando usado para implementação inicial
+    //.then(res => res.json())
+
+}
