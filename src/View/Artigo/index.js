c1cb4b008b2cefc146e4d7ec1a1394fb2b69e519
Victor-Andrade <victor.hugooo@hotmail.com.br>
2020-11-12 12:34:28 -0300

    Melhorias na exibição de artigos

--- src/View/Artigo/index.js
+++ src/View/Artigo/index.js
@@ -1,25 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import styles from "./styles";
+
+let separaTags = (tags) => tags.join(", ");
 
 export default function Artigo({ route }) {
   const artigoRecebido = route.params.artigo;
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      {/* {console.log(artigoRecebido)} */}
+      <StatusBar style="light" />
 
-      <Text style={styles.title} id="title">
-        {artigoRecebido.title}
-      </Text>
 
-      <Text style={styles.article}>{artigoRecebido.content}</Text>
+      <ScrollView style={styles.articleContainer}>
+        <View style={styles.titleContainer}>
 
-      <Text style={styles.tags} id="tag">
-        {artigoRecebido.tags}
-      </Text>
+          <Text style={styles.title} id="title">
+            {artigoRecebido.title}
+          </Text>
+        </View>
 
+        <Text style={styles.article}>{artigoRecebido.content}</Text>
+
+      </ScrollView>
+
+      <View style={styles.tagsContainer}>
+
+        <Text style={styles.tags} id="tag">
+          Tags: {separaTags(artigoRecebido.tags)}
+        </Text>
+      </View>
     </View>
   );
 }
--- src/View/Artigo/styles.js
+++ src/View/Artigo/styles.js
@@ -4,22 +4,54 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingVertical: 0,
+    paddingHorizontal: 0,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "space-between",
+    borderRadius: 0
+  },
+  titleContainer: {
+    margin: 0,
+    backgroundColor: "#f7b20d",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignSelf: "stretch",
+    flex: 1,
+    flexBasis: 125,
   },
   title: {
     alignContent: "center",
-    fontSize: 20,
+    color: "#fff",
+    fontSize: 28,
     fontWeight: "bold",
   },
+  articleContainer: {
+    flex: 0,
+  },
   article: {
+    flex: 4,
     textAlign: "justify",
+    fontSize: 18,
+    lineHeight: 24,
+    paddingHorizontal: 2,
+    margin: 30,
+  },
+  tagsContainer: {
+    flexDirection: "row",
+    flex: 0,
+    flexBasis: 45,
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f7b20d",
   },
   tags: {
-    marginBottom: 5,
+    flex: 0,
+    color: "#fff",
+
   },
 });
 
